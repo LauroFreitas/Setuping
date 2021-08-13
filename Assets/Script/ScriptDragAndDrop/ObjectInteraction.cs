08003109c37f8e125cac52b418293a759b1c72f2
Lauro Henrique Santana de Freitas <lauroh.sfreitas@gmail.com>
2021-08-13 18:45:43 -0300

    Restore Project

---
+++ Assets/Script/ScriptDragAndDrop/ObjectInteraction.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectInteraction : MonoBehaviour
+{
+    public GameObject obj;
+    
+    public void VirarObj()
+    {
+        obj.transform.Rotate(0,30,0);
+    }
+
+}
---
+++ Assets/ScriptableObjects/Mesa/ItenDisplay.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItenDisplay : MonoBehaviour
+{
+    public ItenShop iten;
+    public Text descricao;
+    public Image imagm;
+    public Text cost;
+
+    private void Start()
+    {
+        descricao.text = iten.descisao;
+        imagm.sprite = iten.imagemIten;
+        cost.text = iten.valor.ToString();
+    }
+}
